487069419cbe5a389187df1082a966e82eeea09c
Marcelo Puebla <marcenp23@gmail.com>
2022-09-08 22:53:04 -0300

    Termino seccion home

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, HostListener } from '@angular/core';
+import { ViewportScroller } from '@angular/common';
 
 @Component({
   selector: 'app-header',
@@ -7,12 +8,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
   showMenu = false;
+  isClickInside = false;
 
-  constructor() {}
+  constructor(private scroller: ViewportScroller, private eRef: ElementRef) {}
 
   ngOnInit(): void {}
 
   toggleMenu() {
     this.showMenu = !this.showMenu;
+  }
+
+  goTo(id: string) {
+    this.scroller.scrollToAnchor(id);
+  }
+
+  @HostListener('document:click', ['$event'])
+  clickout(event: any) {
+    if (this.eRef.nativeElement.contains(event.target)) {
+      this.isClickInside = true;
+    } else {
+      if (this.isClickInside && this.showMenu) this.toggleMenu();
+      this.isClickInside = false;
+    }
   }
 }
