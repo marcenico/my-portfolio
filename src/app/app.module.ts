36747639ed089e209bc3fcb5eb1465d6b443fadf
Marcelo Puebla <marcenp23@gmail.com>
2023-04-24 20:50:19 -0300

    Created o-grid scss file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HeaderComponent } from './components/header/header.component';
-import { AboutComponent } from './components/about/about.component';
+import { HomeComponent } from './components/home/home.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 import { SkillsComponent } from './components/skills/skills.component';
 import { DropdownComponent } from './shared/dropdown/dropdown.component';
+import { PortfolioComponent } from './components/portfolio/portfolio.component';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, FooterComponent, HeaderComponent, PageNotFoundComponent, AboutComponent, SkillsComponent, DropdownComponent],
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    FooterComponent,
+    HeaderComponent,
+    PageNotFoundComponent,
+    AboutComponent,
+    SkillsComponent,
+    DropdownComponent,
+    PortfolioComponent
+  ],
   imports: [BrowserModule, BrowserAnimationsModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
