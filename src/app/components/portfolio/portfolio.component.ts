0f03914bd801623fa3dc271ad956a76a369db262
Marcelo Puebla <marcenp23@gmail.com>
2023-06-12 22:12:04 -0300

    Fix - Fixed lang chang on portfolio project descriptions

--- src/app/components/portfolio/portfolio.component.ts
+++ src/app/components/portfolio/portfolio.component.ts
@@ -26,7 +26,19 @@ export class PortfolioComponent implements OnInit {
     });
   }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.translateService.onLangChange.subscribe(({ translations }) => {
+      this.portfolioItems = [
+        {
+          image: 'assets/gifs/project-world-cup.gif',
+          title: `${translations.portfolio.items[0].title}<a class="o-text--h4 o-text--first-color" href="https://latinad.com/" target="_blank">LatinAd</a>`,
+          description: `${translations.portfolio.items[0].description}`,
+          pageLink: undefined,
+          gitHubLink: undefined
+        }
+      ];
+    });
+  }
 
   ngAfterViewInit(): void {
     const targetElement = document.getElementById('portfolio');
