b20960cbbeb308f63b6e135ab12f956111f06661
Marcelo Puebla <marcenp23@gmail.com>
2023-12-23 00:23:53 -0300

    feat(fade): improved anim

--- src/app/animations/animations.ts
+++ src/app/animations/animations.ts
@@ -2,9 +2,9 @@ import { animate, state, style, transition, trigger } from '@angular/animations'
 
 export const fadeAnimation = [
   trigger('fadeAnimation', [
-    state('hidden', style({ opacity: 0.75, transform: 'translateY(50px)' })),
+    state('hidden', style({ opacity: 0.75, transform: 'translateY(20px)' })),
     state('visible', style({ opacity: 1, transform: 'translateY(0)' })),
-    transition('hidden => visible', animate('300ms ease')),
-    transition('visible => hidden', animate('300ms ease'))
+    transition('hidden => visible', animate('400ms ease')),
+    transition('visible => hidden', animate('400ms ease'))
   ])
 ];
