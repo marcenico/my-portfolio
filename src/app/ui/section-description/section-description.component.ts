ad73cebb5ef030e49ff35109ab36c528d6dbf301
Marcelo Puebla <marcenp23@gmail.com>
2024-03-08 22:59:22 -0300

    feat(seo): improvments

--- src/app/ui/section-description/section-description.component.ts
+++ src/app/ui/section-description/section-description.component.ts
@@ -2,7 +2,7 @@ import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-section-description',
-  template: '<p class="o-text o-text--md o-mb-2">{{ section | translate }}</p>'
+  template: '<h2 class="o-text o-text--md o-mb-2">{{ section | translate }}</h2>'
 })
 export class SectionDescriptionComponent {
   @Input() section!: any;
--- src/app/ui/section-title/section-title.component.ts
+++ src/app/ui/section-title/section-title.component.ts
@@ -2,7 +2,7 @@ import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-section-title',
-  template: '<p class="o-text o-text--h2 o-text--semi-bold o-mb-1-5">{{ section | translate }}</p>'
+  template: '<h1 class="o-text o-text--h2 o-text--semi-bold o-mb-1-5">{{ section | translate }}</h1>'
 })
 export class SectionTitleComponent {
   @Input() section!: any;
